92e9bd7a97025c046afec0fe6f805113c9e0ec7f
ayeshaaamir <ayeshaaamir38@gmail.com>
2025-01-18 20:14:07 +0000

    unsubscribed event..

--- netflix-gpt/src/components/Header.jsx
+++ netflix-gpt/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, displayName, email, photoURL } = user;
         dispatch(
@@ -28,6 +28,7 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unsubscribe();
   }, []);
 
   const handleSignOut = () => {
